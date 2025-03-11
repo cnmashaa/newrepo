5ae864164ad68c96b6ae17e6f8dcab33e61edf09
cnmashaa <cnmashaa@gmail.com>
2025-03-11 10:05:22 +0200

    Intro to python

---
+++ intro.py
@@ -0,0 +1,10 @@
+age = 25
+height = 5.9
+name = "Char"
+is_student = True
+
+#check data type 
+print(type(age)) #Output: <class 'int'>
+print(type(height))
+print(type(name))
+print(type(is_student))
